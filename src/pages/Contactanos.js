240974c0533b353c2717eff54006425147768199
bangg1528 <naranjobastian75@gmail.com>
2023-07-12 20:35:22 -0400

    prueba

--- src/pages/Contactanos.js
+++ src/pages/Contactanos.js
@@ -4,6 +4,7 @@ import Form from '../components/Form'
 export default function Contactanos() {
   return (
     <div>
+      <h2>Completa el formulario y uno de nuestros asesores se contactará contigo.</h2>
       <Form/>
     </div>
   )
